a438d2ec86e9db7f576ff86733f8a55c137ff9b0
Hakim <njim.hakimnjim1992@gmail.com>
2020-02-26 23:37:21 +0100

    update playerMvt

--- EducatifGame/Assets/PlayerMvt.cs
+++ EducatifGame/Assets/PlayerMvt.cs
@@ -16,6 +16,7 @@ public class PlayerMvt : MonoBehaviour
     public float gravity = 20.0f;
     public Animator anim;
     private Vector3 moveDirection = Vector3.zero;
+    public float rotationspeed;
 
     void Start()
     {
@@ -29,22 +30,63 @@ public class PlayerMvt : MonoBehaviour
             // We are grounded, so recalculate
             // move direction directly from axes
 
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-            moveDirection *= speed;
-            anim.SetFloat("zSpeed", Input.GetAxis("Vertical"));
+            //moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            
+           // moveDirection = transform.TransformDirection(moveDirection);
 
+            
+            //    transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal"), 0));
+            // characterController.Move(transform.forward * Input.GetAxis("Vertical") * Time.deltaTime);
+            // transform.Rotate(0, Input.GetAxis("Horizontal")*rotationspeed*Time.deltaTime, 0);
+            LockOnTarget();
+            if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+            {
+                characterController.Move(transform.forward * speed * Time.deltaTime);
+                anim.SetFloat("zSpeed", 1);
+            }
+            else
+            {
+                anim.SetFloat("zSpeed", 0);
+            }
             if (Input.GetButton("Jump"))
             {
                 moveDirection.y = jumpSpeed;
+                characterController.Move(moveDirection * speed * Time.deltaTime);
+                anim.SetTrigger("jump");
             }
+            
+            
         }
+        else
+        {
+            characterController.Move(moveDirection*speed * Time.deltaTime);
+        }
+       
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        
+        //characterController.Move(transform.forward* Time.deltaTime);
 
         // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
         // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
         // as an acceleration (ms^-2)
-        moveDirection.y -= gravity * Time.deltaTime;
+
 
         // Move the controller
-        characterController.Move(moveDirection * Time.deltaTime);
+      
+
+    }
+    void LockOnTarget()
+    {
+        Vector3 test = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+
+        if (test- Vector3.zero != Vector3.zero)
+        {
+            Quaternion lookRotation = Quaternion.LookRotation(test);
+            Vector3 rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * rotationspeed).eulerAngles;
+            transform.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+        }
+       
+       
     }
 }
